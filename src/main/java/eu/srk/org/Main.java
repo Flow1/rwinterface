37fccf6fabfb04809e1d170637227636c3760519
thex <thex@10.0.2.15>
2021-03-24 16:15:48 +0100

    fgfg

--- src/main/java/eu/srk/org/Main.java
+++ src/main/java/eu/srk/org/Main.java
@@ -33,7 +33,7 @@ public class Main {
 
 		logs.logInfo("Start application: " + server + ":" + portListen + " "
 				+ portWrite);
-//dsd d
+//dsd d fgfgfg
 		if (server == null) {
 			logs.logError("Servername in properties not found");
 			System.exit(0);
